aa6a663696019613bbfe251f6d5b94ddfc04ea8e
Nicolas Adamini <nicolas.adamini@iwd.io>
2020-11-26 20:51:11 +0100

    refacto: replace json by dart files only en and fr yet

--- lib/example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:i18n_iso_countries/i18n_iso_countries.dart';
-
-void main() async {
-  final localizedCountryNames = await getLocalizedCountryNames('ru');
-  print(localizedCountryNames);
-}
--- lib/i18n_iso_countries.dart
+++
@@ -1,3 +0,0 @@
-library i18n_iso_countries;
-
-export 'package:i18n_iso_countries/src/countries.dart';
